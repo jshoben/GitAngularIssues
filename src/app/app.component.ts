02999bd43380c4a55dbccb10eed1d8d1c69c322b
jshoben <jshoben@gmail.com>
2018-09-20 13:56:25 -0500

    Displaying issues to page after clicking on search

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'GitAngularIssues';
+  response: any;
+
+  constructor(private http: HttpClient) {
+  }
+
+  ngOnInit() {
+  }
+
+  search() {
+    this.http.get('https://api.github.com/repos/Angular/Angular/issues')
+      .subscribe((response) => {
+        this.response = response;
+        console.log(response);
+      });
+  }
 }
